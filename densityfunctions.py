f1d2bd91a7f04f187f1783f5112ddbd22e86627d
Manish <manish97ravula@gmail.com>
2015-09-13 18:51:10 +0530

    PDF and CDF functions for plotting and obtaining vectors

---
+++ densityfunctions.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def gen_cdf(rand_seq,no_bins,plotsig):
+    dist_hist = np.histogram(rand_seq,no_bins);
+    cdf_freq_val = np.cumsum(dist_hist[0]);
+    cdf_xvals=dist_hist[1];
+    cdf_freq_val = np.insert(cdf_freq_val,0,0)/np.max(cdf_freq_val);
+    if plotsig==1:
+        plt.figure()
+        plt.plot(cdf_xvals,cdf_freq_val)
+    return (cdf_freq_val,cdf_xvals)
+
+def gen_pdf(seq,no_bins,plotsig):
+    pdf = np.histogram(seq,no_bins);
+    pdf_val = pdf[0];
+    pdf_xval = pdf[1];
+    pdf_xval = np.delete(pdf_xval,0);
+    if plotsig==1:
+        plt.figure()
+        plt.plot(pdf_xval,pdf_val)
+    return (pdf_val,pdf_xval)
